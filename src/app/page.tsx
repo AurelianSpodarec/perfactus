9ffe56d7f4edccfc8e7efbcb5eaf181873fff774
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-03-25 09:06:13 +0100

    fix: Fixed TypeScript errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,6 +26,7 @@ export default function Home() {
           Go to Path
         </Button>
       </div>
+      {/* <Link href="hi>Hi</Link> */}
 
 
 
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,30 +1,55 @@
 "use client";
+
 import React, { forwardRef } from "react";
-import { Slot } from "@radix-ui/react-slot";
 import { VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "./_helpers/buttonVariants";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 
-export interface LinkProps {
-  // target, href, path?
-}
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
-  tag?: "button" | "link";
-  label?: string;
-  asChild?: boolean;
-  isLoading?: boolean;
-  loadingPosition?: "" | "left" | "right";
-  icon?: React.ReactNode;
-  iconPosition?: "left" | "right";
-  block?: boolean;
-  kind?: "solid";
-  isLoadingText?: boolean;
+function renderSpinner() {
+  return (
+    <svg
+      className="animate-spin"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
+    </svg>
+  );
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+type ButtonBaseProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  tag?: "button";
+};
+
+type LinkBaseProps = Omit<LinkProps, "passHref"> &
+  React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+    tag: "link";
+    href: string;
+  };
+
+export type ButtonProps = (ButtonBaseProps | LinkBaseProps) &
+  VariantProps<typeof buttonVariants> & {
+    label?: string;
+    children?: React.ReactNode;
+    icon?: React.ReactNode;
+    iconPosition?: "left" | "right";
+    isLoading?: boolean;
+    loadingPosition?: "" | "left" | "right";
+    block?: boolean;
+    kind?: "solid";
+  };
+
+type ButtonRef = HTMLButtonElement | HTMLAnchorElement;
+
+const Button = forwardRef<ButtonRef, ButtonProps>(
   (
     {
       tag = "button",
@@ -32,71 +57,45 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       children,
       icon,
       iconPosition = "left",
-      block = false,
       isLoading = false,
       loadingPosition = "",
+      block = false,
       kind = "solid",
-      variant,
-      size,
-      asChild = false,
-      isLoadingText = false,
-      className,
+      type = "button",
       ...props
     },
     ref
   ) => {
-    const Comp = as === "link" ? Link : "button";
-    const isFullLoading = isLoading && loadingPosition === "";
-
-    function renderSpinner() {
-      return (
-        <svg
-          className="animate-spin"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-        >
-          <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
-        </svg>
-      );
-    }
+    const isLink = tag === "link";
+    const Tag = (isLink ? Link : "button") as any;
 
     return (
-      <Comp
+      <Tag
         ref={ref}
-        {...(tag === "button" ? { type: "button" } : {})}
-        disabled={isFullLoading}
+        {...(isLink ? (props as LinkBaseProps) : (props as ButtonBaseProps))}
+        {...(!isLink && { type })}
         aria-busy={isLoading}
-        className={cn(
-          buttonVariants({ variant, kind, size }),
-          `relative inline-flex items-center justify-center ${isLoading && loadingPosition === "" ? "text-transparent" : ""}`
+        className={cn(`relative inline-flex items-center justify-center 
+          ${isLoading && loadingPosition === "" ? "text-transparent" : ""} 
+          ${block ? "w-full" : ""}`
         )}
-        {...props}
       >
-        <>
-          {isLoading && loadingPosition === "" && (
-            <span className="contents">
-              <span className="absolute left-1/2 -translate-x-1/2 flex visible text-black/30">
-                {renderSpinner()}
-              </span>
-            </span>
-          )}
+        {isLoading && loadingPosition === "" && (
+          <span className="absolute left-1/2 -translate-x-1/2 flex visible text-black/30">
+            {renderSpinner()}
+          </span>
+        )}
 
-          {isLoading && loadingPosition !== "" && loadingPosition === "left" && (renderSpinner())}
-          {children ? children : label}
-          {isLoading && loadingPosition !== "" && loadingPosition === "right" && (renderSpinner())}
+        {isLoading && loadingPosition === "left" && renderSpinner()}
+        {children || label}
+        {isLoading && loadingPosition === "right" && renderSpinner()}
 
-          {icon && <span className="w-4 h-4">{icon}</span>}
-        </>
-      </Comp>
+        {icon && <span className="w-4 h-4">{icon}</span>}
+      </Tag>
     );
   }
 );
 
 Button.displayName = "Button";
+
+export { Button };
