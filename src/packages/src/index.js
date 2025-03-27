345b945b84ec5ffdb5a6caed20facbd554b51e37
Turbobot <turbobot@vercel.com>
2025-03-27 22:46:40 +0100

    feat(create-turbo): apply official-starter transform

---
+++ src/packages/src/index.js
@@ -0,0 +1,13 @@
+export * from './components/index.js';
+
+
+
+Input/Input password
+Button 
+
+// 
+
+// Component will live in 
+
+// ui/components (just like shadcn)
+// input file for css variables for tailiwndcss needed (will be appended at the bottom of the file, or a new one will be created)
