0efa95cdbbabee81326e1c65d2e099aa26c4cdcd
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-04-13 02:16:56 +0200

    chore: moved renderSpinner function in its own file

--- packages/ui/button/src/button.tsx
+++ packages/ui/button/src/button.tsx
@@ -9,25 +9,7 @@ import { VariantProps } from "class-variance-authority";
 import { cn } from "../../../lib/src/utils";
 
 import { buttonVariants } from "./buttonVariants";
-
-function renderSpinner() {
-  return (
-    <svg
-      className="animate-spin size-4 text-pink-500"
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      viewBox="0 0 24 24"
-    >
-      <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
-    </svg>
-  );
-}
+import { renderSpinner } from "./spinner";
 
 type ButtonBaseProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
